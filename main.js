e9ae147e854aa4eafe2108f22f1ad6f8c453a08d
fedPass <federica.passanante@gmail.com>
2019-12-16 13:13:13 +0100

    impostato pseudo codice

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    
+
+    //al click sul button devo
+    //leggere il value dell'input
+    //fare una chiamata API per recuperare i titoli in database
+    //confrontare il titolo cercato con titoli in lista
+    // se le parole cercate sono contenute nel titolo in lista mostrarlo
+
+    // Esempio chiamata:
+    // https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro
+
+    //link base dell'api
+    var api_basic = 'https://developers.themoviedb.org/3';
+    //noi useremo la chiamata --> /search/movie
+
+    //Passiamo come parametri api_key e query
+    //API Key: 545efb8b9373f473ca0a15eafe64304c (key ottenuta da moviedb)
+    //query dovrebbe coindidere con il value passato dall'utente
+
 });
