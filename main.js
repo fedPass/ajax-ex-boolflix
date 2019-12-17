631fe0b3dcbd65cd17164c96a245dd917c1c3ce9
fedPass <federica.passanante@gmail.com>
2019-12-17 21:00:21 +0100

    prova ricerca serie tv

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     //----dichiaro le variabili globali
     var template_html = $("#film-template").html();
     var template_function = Handlebars.compile(template_html);
+    var api_base = 'https://api.themoviedb.org/3';
+    var api_key = '545efb8b9373f473ca0a15eafe64304c';
 
     //reazione al click
     $('#search_container button').click(nuova_ricerca);
@@ -15,39 +17,39 @@ $(document).ready(function(){
 
     function nuova_ricerca(){
         //leggere il value dell'input
-        var film_searched = $('#search_container input').val();
-        console.log(film_searched);
+        var typed_text = $('#search_container input').val();
+        console.log(typed_text);
         //controllo che sia stato digitato qualcosa
-        if (film_searched.length != 0) {
+        if (typed_text.length != 0) {
             //prima di tutto devo cancellare tutte le card eventualmente già visibili (perchè se no mi aggiunge valori sotto)
             $('.card').remove();
             $('#error').remove();
             //svuoto il value dell'input
             $('#search_container input').val('');
-            cerca_api(film_searched);
+            cerca_film(typed_text);
+            cerca_serie(typed_text);
         } else {
             display_error();
         }
     }
 
-    function cerca_api (film_searched) {
-        var api_base = 'https://api.themoviedb.org/3';
-        var api_key = '545efb8b9373f473ca0a15eafe64304c';
-        //fare una chiamata API per recuperare i titoli in database
+    function cerca_film (typed_text) {
+        //fare una chiamata API per recuperare i titoli dei film in database
         $.ajax({
-            // 'url':'https://api.themoviedb.org/3/search/movie?api_key=545efb8b9373f473ca0a15eafe64304c&query=' + film_searched,
+            // 'url':'https://api.themoviedb.org/3/search/movie?api_key=545efb8b9373f473ca0a15eafe64304c&query=' + typed_text,
             'url': api_base + '/search/movie',
             'data' : {
                 'api_key': api_key,
-                'query': film_searched
+                'query': typed_text
             },
             'method':'get',
-            'success': function(response){
+            'success': function(response_film){
                 //per ogni risultato della chiamata in lista recupera Titolo, Titolo Originale, Lingua, Voto
-                var film_list = response.results;
+                var film_list = response_film.results;
                 if (film_list.length != 0 ) {
                     stampa_risultati(film_list);
                 } else {
+                    //se non trova nulla (da sistemare dopo)
                     display_error();
                 }
 
@@ -61,25 +63,65 @@ $(document).ready(function(){
         });
     }
 
+    function cerca_serie (typed_text) {
+        //fare una chiamata API per recuperare i titoli delle serie in database
+        $.ajax({
+            // 'url':'https://api.themoviedb.org/3/search/movie?api_key=545efb8b9373f473ca0a15eafe64304c&query=' + typed_text,
+            'url': api_base + '/search/tv',
+            'data' : {
+                'api_key': api_key,
+                'query': typed_text
+            },
+            'method':'get',
+            'success': function(response_serieTv){
+                //per ogni risultato della chiamata in lista recupera Titolo, Titolo Originale, Lingua, Voto
+                var serieTv_list = response_serieTv.results;
+                if (serieTv_list.length != 0 ) {
+                    stampa_risultati(serieTv_list);
+                } else {
+                    //se non trova nulla (da sistemare dopo)
+                    display_error();
+                }
+            },
+            'error': function(error){
+                //se non inserisci nulla
+                if (error.status == 422) {
+                    display_error()
+                }
+            }
+        });
+    }
+
     function stampa_risultati(risultati){
+        $('.etichetta_sezione h2').show();
         //estraggo info su ogni film
         for (var i = 0; i < risultati.length; i++) {
-            var titolo = risultati[i].title;
+            var titoloSerieTv = risultati[i].name;
+            var titoloFilm = risultati[i].title;
             var titolo_originale = risultati[i].original_title;
             var lingua = risultati[i].original_language;
             var voto = risultati[i].vote_average;
             var numero_stelle = Math.ceil(voto/2);
-            console.log(titolo + ': ' + numero_stelle);
+            console.log(titoloFilm + ' (film): ' + numero_stelle);
+            console.log(titoloSerieTv + ' (serie): ' + numero_stelle);
             var bandiera = seleziona_bandiera(lingua);
             var stelle = crea_stelle(numero_stelle);
-            var context = {
-                'title':titolo,
+            var context_film = {
+                'title':titoloFilm,
                 'original_title':titolo_originale,
                 'lang':bandiera,
                 'rating':stelle
             };
-            var html = template_function(context);
-            $('#display_container').append(html);
+            var context_serieTv = {
+                'title':titoloSerieTv,
+                'original_title':titolo_originale,
+                'lang':bandiera,
+                'rating':stelle
+            };
+            var html_film = template_function(context_film);
+            $('#display_film').append(html_film);
+            var html_serieTv = template_function(context_serieTv);
+            $('#display_serieTv').append(html_serieTv);
         }
     }
 
@@ -134,7 +176,7 @@ $(document).ready(function(){
     }
 
     function display_error() {
-        $('#display_container').append(`<div id="error">
+        $('#display_film').append(`<div id="error">
             <strong>Non hai inserito un titolo valido oppure il film non è presente in database</strong>
         </div>`);
     }
